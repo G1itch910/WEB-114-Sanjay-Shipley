31a96b8063a937c8b1dc21b5675a1f5a0edeff4f
Sanjay Shipley <733sas21@students.olatheschools.com>
2026-03-15 12:29:00 -0500

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,25 @@
+"use strict";
+const answer = prompt("What is the square root of 25?");
+if (answer.toLowerCase() == "5") {
+    console.log("Correct!");
+} else {
+    console.log("Incorrect, the square root of 25 is 5.");
+}
+const answer2 = prompt("What shape has an infinite number of sides?");
+if (answer2.toLowerCase() == "circle") {
+    console.log("Correct!");
+} else {
+    console.log("Incorrect, Circles have in infinite number of sides");
+}
+const answer3 = prompt("What state is known as the sunflower state?");
+if (answer3.toLowerCase() == "kansas") {
+    console.log("Correct!");
+} else {
+    console.log("Incorrect, Kansas is known as the sunflower state.");
+}
+const answer4 = prompt("what is 12 * 6?");
+if (answer4.toLowerCase() == "72") {
+    console.log("Correct!");
+} else {
+    console.log("Incorrect, the answer is 72.");
+}

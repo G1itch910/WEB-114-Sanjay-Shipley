4916783424b475ab199c81a8561b7dacedc17216
Sanjay Shipley <733sas21@students.olatheschools.com>
2026-03-17 14:26:29 -0500

    Add files via upload

---
+++ wages/wages.js
@@ -0,0 +1,27 @@
+"use strict";
+let confirmChoice = false // this variable is going to be used throughout the program
+let hourlyWage
+let hoursWorked
+while (confirmChoice == false) {
+    hourlyWage = prompt("What is your hourly wage?");
+    hourlyWage = Number(hourlyWage.trim());
+    confirmChoice = confirm(`You entered ${hourlyWage}, is that correct?`)
+}
+confirmChoice = false
+while (confirmChoice == false) {
+    hoursWorked = prompt("How many hours did you work this week?");
+    hoursWorked = Number(hoursWorked.trim());
+    confirmChoice = confirm(`You entered ${hoursWorked}, is that correct?`);
+}
+const grossPay = hoursWorked * hourlyWage;
+const taxes = grossPay * 0.1;
+const netPay = grossPay - taxes;
+console.log(`You earned $${netPay.toFixed(2)} this week!`);
+
+if (hoursWorked > 40) {
+    console.log("You worked overtime this week!");
+} else if (hoursWorked == 40) {
+    console.log("You worked exactly 40 hours");
+} else {
+    console.log("No overtime this week.");
+}
